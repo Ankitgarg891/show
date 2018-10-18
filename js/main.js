17918076c76de1fc28f064c7857363efdc3d9093
nishutosh <nishutosh002@gmail.com>
2018-10-18 22:46:37 +0530

    html

--- js/main.js
+++ js/main.js
@@ -12,7 +12,10 @@
                              $(".effect-text").css("margin-top", "26vh")
                              $(".effect-text").html('<svg xmlns="http://www.w3.org/2000/svg" width="254" height="254" viewBox="0 0 24 24" fill="none" stroke="tomato" stroke-width="0.2" stroke-linecap="round" stroke-linejoin="round" class="heart"><path d="M20.84 4.61a5.5 5.5 0 0 0-7.78 0L12 5.67l-1.06-1.06a5.5 5.5 0 0 0-7.78 7.78l1.06 1.06L12 21.23l7.78-7.78 1.06-1.06a5.5 5.5 0 0 0 0-7.78z"></path></svg>');
                              $(".effect-text").css("display", "block")
-                             tubelightEffect()
+                             tubelightEffect();
+                             setTimeout(function() {
+                                 $(".heart").attr("opacity", 0)
+                             }, 6000);
                          });
                      })
                  });
